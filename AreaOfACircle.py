8142abafbb9207f983e7417ebc605a4b2074192f
Neina Cichon <ncichon@cincinnatistate.edu>
2021-06-24 17:13:03 -0400

    program

---
+++ AreaOfACircle.py
@@ -0,0 +1,11 @@
+#--------------------------------------------
+# Name: Neina Cichon
+# Abstract: Area of a Circle
+# Date: 6/2/2020
+#--------------------------------------------
+
+dblPi = float(3.14)
+dblRadius = float(input("Enter Radius of Circle: "))
+dblArea = dblPi*(dblRadius**2)
+
+print("The area of the circle is ", dblArea, " inches.")
